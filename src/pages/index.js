3e76ec910edc21efde103212ce2734bd9c75b8a9
Brian Diaz <brianjdiaz@gmail.com>
2019-09-27 14:49:08 -0400

    Init Gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import styled from 'styled-components'
+
+export default () => <Thing>Hello world!</Thing>
+
+const Thing = styled.div`
+  height: 100vh;
+  width: 100%;
+`
